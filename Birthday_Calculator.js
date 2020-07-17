29612422168c932a10e754d44a576e58c4134009
Grace <wanenegrace@gmail.com>
2020-07-17 18:48:36 +0300

    Added a line to call the dateCalculator function

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -6,14 +6,15 @@ var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturda
 // Calculate the day of the week that the user was born
 function dayCalculator() {
   birthDate = prompt("Please enter your birth date in this format dd/mm/yyyy");
-  confirm("Please confirm that you were born on " + birthDate);
+  // confirm("Please confirm that you were born on " + birthDate);
   var dateComponents = birthDate.split("/"); // Split date into day, month, year
   console.log(dateComponents);
   var DD = parseInt(dateComponents[0]);
   var MM = parseInt(dateComponents[1]);
   var YY = parseInt(dateComponents[2]);
-
-  var newDate = new Date(YY, MM-1, DD); // Normally we count months of the year from 1 but we need to deduct one because computers count from 0
-  return .getDay();
-
+  var newDate = new Date(YY, MM-1, DD); // Deduct 1 because computers count from 0 so January will be 0
+  weekDay = weekDays[newDate.getDay()];
+  console.log(weekDay)
 }
+
+dayCalculator();
