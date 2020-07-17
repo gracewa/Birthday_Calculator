f26b3279b42bb3edbebf7fe357af8833607f015e
Grace <wanenegrace@gmail.com>
2020-07-17 17:28:57 +0300

    Adedd prompt to ask user to confirm their birth date and method to figure out century digits

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -1,9 +1,11 @@
-var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 // Calculate the day of the week that the user was born
-
 function dayCalculator() {
-  birthDate=prompt("Please enter your birth date in this format dd/mm/yyyy");
-  
+  birthDate = prompt("Please enter your birth date in this format dd/mm/yyyy");
+  confirm("Please confirm that you were born on " + birthDate);
+  // Figure out the century
+  CC = birthDate.charAt(0) + birthDate.charAt(1);
+  console.log(CC);
 }
