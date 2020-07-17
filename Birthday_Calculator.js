12c48c00f922a7671516496d34b5773b047afc20
Grace <wanenegrace@gmail.com>
2020-07-17 17:15:55 +0300

    Created calculator function and added prompt method to ask user for their birth date

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -1,2 +1,9 @@
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+// Calculate the day of the week that the user was born
+
+function dayCalculator() {
+  birthDate=prompt("Please enter your birth date in this format dd/mm/yyyy");
+  
+}
