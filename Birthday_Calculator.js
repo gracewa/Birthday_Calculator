f18eff3c54c07893f31716c18e05d86ac2e68973
Grace <wanenegrace@gmail.com>
2020-07-17 18:40:17 +0300

    Switching from formula calculation to getDay method

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -1,12 +1,19 @@
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var dateComponents = []
+var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 // Calculate the day of the week that the user was born
 function dayCalculator() {
   birthDate = prompt("Please enter your birth date in this format dd/mm/yyyy");
   confirm("Please confirm that you were born on " + birthDate);
-  CC = parseInt(birthDate.charAt(6) + birthDate.charAt(7)); // Figure out the century digits
-  YY = parseInt(birthDate.charAt(8) + birthDate.charAt(9));//Figure out the Year digits
-  MM = parseInt(birthDate.charAt(3) + birthDate.charAt(4));//Figure out the Month
-  console.log();
+  var dateComponents = birthDate.split("/"); // Split date into day, month, year
+  console.log(dateComponents);
+  var DD = parseInt(dateComponents[0]);
+  var MM = parseInt(dateComponents[1]);
+  var YY = parseInt(dateComponents[2]);
+
+  var newDate = new Date(YY, MM-1, DD); // Normally we count months of the year from 1 but we need to deduct one because computers count from 0
+  return .getDay();
+
 }
