f048739b906fda53876e6d06a729ae1b63e628a3
Grace <wanenegrace@gmail.com>
2020-07-18 20:44:12 +0300

    Added if statement to check date validity and change input type for date field to text

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -6,20 +6,29 @@ var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 // Calculate the day of the week that the user was born
 function dayCalculator() {
   var birthDate = document.getElementById("userBirthday").value;
-  var dateComponents = birthDate.split("-"); // Split date into day, month, year
+  var dateComponents = birthDate.split("/"); // Split date into day, month, year
   console.log(dateComponents);
-  var DD = parseInt(dateComponents[2]);
+  var DD = parseInt(dateComponents[0]);
   var MM = parseInt(dateComponents[1]);
-  var YY = parseInt(dateComponents[0]);
-  var newDate = new Date(YY, MM-1, DD); // Deduct 1 because computers count from 0 so January will be 0
-  weekDayIndex = newDate.getDay();
-  var gender = document.getElementById("userGender").value;
-  if (gender === "Female") {
-    var akanName = femaleNames[weekDayIndex];
-    alert("Your Akan Name is " + akanName);
+  var YY = parseInt(dateComponents[2]);
+  if (DD<=0 || DD>31) {
+    alert("Invalid Date. Please re-enter your birthday in the following format: dd/mm/yyyy");
+  } else if (MM<=0 || MM>12) {
+    alert("Invalid Date. Please re-enter your birthday in the following format: dd/mm/yyyy");
   } else {
-    var akanName = maleNames[weekDayIndex];
-    alert("Your Akan Name is " + akanName);
+    var newDate = new Date(YY, MM-1, DD); // Deduct 1 because computers count from 0 so January will be 0
+    weekDayIndex = newDate.getDay();
+    var gender = document.getElementById("userGender").value;
+    if (gender === "Female") {
+      var akanName = femaleNames[weekDayIndex];
+      alert("Your Akan Name is " + akanName);
+    } else {
+      var akanName = maleNames[weekDayIndex];
+      alert("Your Akan Name is " + akanName);
+    }
   }
+
+
+
 
 }
