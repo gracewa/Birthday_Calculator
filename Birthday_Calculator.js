dca4fc8dbc251af2180ba701c807f677c36ecdac
Grace <wanenegrace@gmail.com>
2020-07-18 17:55:52 +0300

    Added Alert on click

--- Birthday_Calculator.js
+++ Birthday_Calculator.js
@@ -5,16 +5,13 @@ var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 
 // Calculate the day of the week that the user was born
 function dayCalculator() {
-  birthDate = prompt("Please enter your birth date in this format dd/mm/yyyy");
-  // confirm("Please confirm that you were born on " + birthDate);
-  var dateComponents = birthDate.split("/"); // Split date into day, month, year
+  var birthDate = document.getElementById("userBirthday").value;
+  var dateComponents = birthDate.split("-"); // Split date into day, month, year
   console.log(dateComponents);
-  var DD = parseInt(dateComponents[0]);
+  var DD = parseInt(dateComponents[2]);
   var MM = parseInt(dateComponents[1]);
-  var YY = parseInt(dateComponents[2]);
+  var YY = parseInt(dateComponents[0]);
   var newDate = new Date(YY, MM-1, DD); // Deduct 1 because computers count from 0 so January will be 0
   weekDay = weekDays[newDate.getDay()];
-  console.log(weekDay)
+  alert(weekDay);
 }
-
-dayCalculator();
